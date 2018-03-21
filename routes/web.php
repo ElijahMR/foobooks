c702cad480cf310397697410a146a3bb124afa40
Elijah <elijah@mystphy.sx>
2018-03-21 19:52:30 +0530

    Week 7 progress.

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,18 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+/**
+ * Misc. pages
+ */
 Route::get('/', 'PageController@welcome');
 Route::get('/about', 'PageController@about');
-
 Route::get('/contact', 'PageController@contact');
 
+/**
+ * Books
+ */
 Route::get('/books', 'BookController@index');
 Route::get('/books/{title}', 'BookController@show');
+
+/**
+ * Practice
+ */
+Route::any('/practice/{n?}', 'PracticeController@index');
